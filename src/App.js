394ea6e8f770f3f1d15e2b4e27aba517ee3131df
Vedant Saini <72696670+AspiringDeveloper3@users.noreply.github.com>
2021-02-01 12:42:36 +0530

    delete and complete animation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,38 @@ import React, { useState } from "react";
 function App() {
   const [inputVal, setInputVal] = useState("");
   const [todos, setTodos] = useState([]);
+  const [isChecked, setChecked] = useState(false);
+  const tickClass = `fas fa-check-square complete-btn`;
+  const crossClass = `fas fa-times cross-btn`;
 
-  function makeTodo() {
+  function makeTodo(event) {
     setTodos((prevValue) => {
       return [...prevValue, inputVal];
     });
+    event.target.parentElement.children[0].value = "";
+  }
+
+  function deleteTodo(event) {
+    event.target.parentElement.classList.add("delete");
   }
 
   function handleChange(event) {
     setInputVal(event.target.value);
+  }
+
+  function completeTodo(event) {
+    setChecked((prevValue) => !prevValue);
+    console.log(event.target.classList[1]);
+    if (event.target.classList[1] === "fa-check-square") {
+      event.target.classList = crossClass;
+      event.target.parentElement.children[0].style.textDecoration =
+        "line-through";
+      event.target.parentElement.children[0].style.background = "#8ea6ad";
+    } else if (event.target.classList[1] === "fa-times") {
+      event.target.classList = tickClass;
+      event.target.parentElement.children[0].style.textDecoration = "none";
+      event.target.parentElement.children[0].style.background = "";
+    }
   }
 
   return (
@@ -27,7 +50,21 @@ function App() {
 
       <ul>
         {todos.map((todo) => {
-          return <li>{todo}</li>;
+          return (
+            <div className="todo-item">
+              <li
+                style={{
+                  textDecoration: "none",
+                  background: "",
+                  transition: `all 0.4s ease`,
+                }}
+              >
+                {todo}
+              </li>
+              <i onClick={completeTodo} className={tickClass}></i>
+              <i onClick={deleteTodo} className="fas fa-trash trash-btn"></i>
+            </div>
+          );
         })}
       </ul>
     </div>
